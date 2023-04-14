3c4e404318a060dd0da3fc87166f14657c440667
omar ortega <oaosolis@gmail.com>
2023-04-13 21:42:14 -0500

    Actualizacion en JS y CSS

--- Lógica de programación/ONE-Challenge/Encriptador de texto/Main.js
+++ Lógica de programación/ONE-Challenge/Encriptador de texto/Main.js
@@ -1,5 +1,11 @@
 
 function  desaparecer(){
     document.getElementById("imgMuneco").style.display="none";
+    document.getElementById("txtOutIndication").style.display="none";
+    aparecerBttn();
+    }
+
+    function aparecerBttn(){
+        document.getElementById("copybttn").style.display="block";
     }
     
